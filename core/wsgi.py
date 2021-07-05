2675c2d86f50da4d724b83e8f033ebab4a5e9690
Katch Vidal <katchvidal@gmail.com>
2021-07-05 13:30:56 -0500

    ADD DJ STATIC Y MODIFY FILE WSGI

--- core/wsgi.py
+++ core/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.production')
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
+
+from dj_static import Cling
+
