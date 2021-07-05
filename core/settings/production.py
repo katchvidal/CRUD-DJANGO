cd927c4e3690c4fdc87a2f52b511f23db795c399
Katch Vidal <katchvidal@gmail.com>
2021-07-05 13:35:45 -0500

    commit static2

--- core/settings/production.py
+++ core/settings/production.py
@@ -22,4 +22,6 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (BASE_DIR, 'static')
